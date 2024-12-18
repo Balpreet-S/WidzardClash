334b7a2285fd7ca91c13c17533d7ee57d8999dac
RalphAchk <ralph.achkar@hotmail.com>
2024-12-18 18:19:48 +0000

    Revert "Path Visualization Test"
    This reverts commit 1d55ff0179c3cdd09af87ee2b7f883aa87a43039.

--- Assets/Scripts/EnemyWizard/EnemySpawner.cs
+++ Assets/Scripts/EnemyWizard/EnemySpawner.cs
@@ -15,20 +15,10 @@ public class EnemySpawner : MonoBehaviour
     private int currentWave = 0;
     private int enemiesToSpawn;
     private int enemiesRemaining;
-    private LineRenderer lr;
 
     void Start()
     {
-        lr = GetComponent<LineRenderer>();
-        lr.positionCount = waypoints.Length;
-        lr.useWorldSpace = true;
-
-        for (int i = 0; i < waypoints.Length; i++)
-        {
-            lr.SetPosition(i, waypoints[i].position);
-        }
-
-        // The rest of your start code (delays, wave starting, etc.)
+        // delay for the first wave spawn
         StartCoroutine(StartWaveAfterDelay());
     }
 
