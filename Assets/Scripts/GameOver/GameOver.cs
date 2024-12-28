d07a89a9372f3f2c16fcab9507ca15d20583895f
joshua82821 <joshuarjayasinghe@gmail.com>
2024-12-28 22:51:19 +0000

    Added Game Over Screen Functionality
    When you click on the restart button, it starts the game from the beginning. When you click quit, you quit the application.

--- Assets/Scripts/GameOver/GameOver.cs
+++ Assets/Scripts/GameOver/GameOver.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    public void restart() // Starts the game from the beginning
+    {
+        SceneManager.LoadScene(0);
+    }
 
+    public void quit() // quits game
+    {
+        Application.Quit();
+    }
 }
