fc2b7988b5f7c1b33a45c34b52e6d55c773272d7
joshua82821 <joshuarjayasinghe@gmail.com>
2025-01-04 23:34:33 +0000

    Update CastleHealthBar.cs

--- Assets/Scripts/Castle/CastleHealthBar.cs
+++ Assets/Scripts/Castle/CastleHealthBar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,6 +12,11 @@ public class CastleHealthBar : MonoBehaviour
 
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
-        _healthbarSprite.fillAmount = currentHealth / maxHealth;    
+        _target = currentHealth / maxHealth;    
+    }
+
+    void Update()
+    {
+        _healthbarSprite.fillAmount = Mathf.MoveTowards(_healthbarSprite.fillAmount, _target, _reduceSpeed * Time.deltaTime);
     }
 }
