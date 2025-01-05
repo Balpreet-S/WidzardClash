85fc2e65f9cdf2bf17f5097c08f8e655f2fdfb5d
joshua82821 <joshuarjayasinghe@gmail.com>
2025-01-05 22:16:41 +0000

    game won screen
    activates game won screen when you press the ultimate skill button

--- Assets/Scripts/XP System/EnemyKilledXP.cs
+++ Assets/Scripts/XP System/EnemyKilledXP.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 //xp system for when enemies are killed (includes skill points)
 public class XPManager : MonoBehaviour
@@ -79,6 +80,9 @@ public class XPManager : MonoBehaviour
             // Destory insted of die 
 
             Debug.Log("Congrats on winning the game!!");
+
+            // Loads the game won scene
+            SceneManager.LoadScene("Game Won");
         }
     }
 
