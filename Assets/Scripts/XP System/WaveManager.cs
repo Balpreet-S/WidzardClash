550117fab898e21adc2ed278f07553b29ddb4c74
Balpreet Singh <singhbalpreet2014@gmail.com>
2025-01-05 20:49:28 +0000

    Delete button for the Base wizard

--- Assets/Scripts/XP System/WaveManager.cs
+++ Assets/Scripts/XP System/WaveManager.cs
@@ -19,10 +19,10 @@ public class WaveManager : MonoBehaviour
     {
         // Wave 1: only the base button is interactable
         baseSkillButton.interactable = true;
-        fireSkillButton.interactable = false;
-        waterSkillButton.interactable = false;
-        earthSkillButton.interactable = false;
-        ULTButton.interactable = false;
+        fireSkillButton.interactable = true;
+        waterSkillButton.interactable = true;
+        earthSkillButton.interactable = true;
+        ULTButton.interactable = true;
     }
 
     public void NextWave()
