8a8a9f09068d2ac49c87a47324ab15f87066c339
RalphAchk <ralph.achkar@hotmail.com>
2025-01-05 11:37:17 +0000

    Revert "Update PauseMenu.cs"
    This reverts commit 623278abc2ae5d0bf060a710910e6428c8291811.

--- Assets/Scripts/Ingame PauseMenu/PauseMenu.cs
+++ Assets/Scripts/Ingame PauseMenu/PauseMenu.cs
@@ -33,6 +33,8 @@ public class PauseMenu : MonoBehaviour
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f; // pauses the game
+        ScoreCounter.HighScoreUpdate();
+        check = true;
     }
 
     public void ResumeGame()
