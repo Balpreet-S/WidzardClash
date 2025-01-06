8b7c6509c558b4ef690c3183b6d303e41fff1faf
Balpreet Singh <singhbalpreet2014@gmail.com>
2025-01-06 03:06:34 +0000

    wincon update

--- Assets/Scripts/GoodWizard/WinCon.cs
+++ Assets/Scripts/GoodWizard/WinCon.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this code is responsable for the win condition
 public class WinCon : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Congrats on finishing the game!");
